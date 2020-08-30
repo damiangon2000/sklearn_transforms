62f419152967dfcbdf7b55aea629479e13d42dcf
Damian Gonzalez <damiangon2000@gmail.com>
2020-08-29 21:13:24 -0300

    agregado profile number

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,6 +1,13 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 
-
+class AddProfileNumber(BaseEstimator, TransformerMixin):       
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        data = X.copy()        
+        data = data.assign(PROFILENUMERICO=data["PROFILE"].map({'beginner_front_end': 7, 'advanced_front_end': 1, 'beginner_backend': 3, 'advanced_backend': 4, 'beginner_data_science': 5, 'advanced_data_science': 6}))
+        return data
 # All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
     def __init__(self, columns):
